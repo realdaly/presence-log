358bc1975e21b70c6f7238b8bfc626c5d3cbacd3
realdaly <0you.hearty0@gmail.com>
2025-04-20 21:36:02 +0300

    complete frontend code for createDayBtn with states for values and onchange events for inputs

--- components/profilepage/CreateDayBtn.jsx
+++ components/profilepage/CreateDayBtn.jsx
@@ -2,12 +2,25 @@
 import { useState } from "react";
 import Modal from "@/components/ui/Modal";
 import createDay from "@/utils/profilepage/createDay";
+import handleNumInput from "@/utils/handleNumInput";
 
 export default function CreateDayBtn({employeeId, year, month, getDays}){
     let [isOpen, setIsOpen] = useState(false);
 
     // states for values
     let [title, setTitle] = useState("");
+    let [timeOff, setTimeOff] = useState(false);
+    let [attendHour, setAttendHour] = useState("");
+    let [attendMin, setAttendMin] = useState("");
+    let [leaveHour, setLeaveHour] = useState("");
+    let [leaveMin, setLeaveMin] = useState("");
+    let [exitHour, setExitHour] = useState("");
+    let [exitMin, setExitMin] = useState("");
+    let [enterHour, setEnterHour] = useState("");
+    let [enterMin, setEnterMin] = useState("");
+    let [dateMonth, setDateMonth] = useState("");
+    let [dateDay, setDateDay] = useState("");
+    let [note, setNote] = useState("");
 
     const submitFunc = async () => {
         if(title){
@@ -30,7 +43,7 @@ export default function CreateDayBtn({employeeId, year, month, getDays}){
             إضافة يوم +
         </button>
         <Modal 
-            title="إضافة يوم"
+            title={`إضافة يوم إلى ${month.title}`}
             sumbitLabel="إضافة"
             isOpen={isOpen}
             setIsOpen={setIsOpen}
@@ -51,6 +64,161 @@ export default function CreateDayBtn({employeeId, year, month, getDays}){
                     onChange={e => setTitle(e.target.value)}
                     data-autofocus
                 />
+                <div className="flex items-center justify-start w-full pt-3 mr-5 select-none">
+                    <input 
+                        id="default-checkbox" 
+                        type="checkbox" 
+                        name="time_off" 
+                        checked={timeOff}
+                        onChange={e => setTimeOff(e.target.checked)}
+                        className="cursor-pointer size-7"
+                    />
+                    <label 
+                        htmlFor="default-checkbox" 
+                        className="cursor-pointer mr-2"
+                    >
+                            يوم إجازة
+                    </label>
+                </div>
+                <div className="pt-5">
+                    <p className="text-center pb-1 font-bold">وقت الحضور:</p>
+                    <div className="w-full flex items-center gap-x-10">
+                        <div>
+                            <p className="text-center pb-2">الحضور:</p>
+                            <div className="flex items-center gap-x-3">
+                                <input
+                                    placeholder="الساعة"
+                                    className="px-4 py-2 bg-comp rounded-xl w-20 disabled:bg-black/50" 
+                                    type="text"
+                                    name="attend_hour"
+                                    onKeyDown={e => handleNumInput(e, setAttendHour)}
+                                    onChange={e => setAttendHour(e.target.value)}
+                                    disabled={timeOff}
+                                    maxLength={2}
+                                    />
+                                <input
+                                    placeholder="الدقيقة"
+                                    className="px-4 py-2 bg-comp rounded-xl w-20 disabled:bg-black/50" 
+                                    type="text"
+                                    name="attend_minute"
+                                    onKeyDown={e => handleNumInput(e, setAttendMin)}
+                                    onChange={e => setAttendMin(e.target.value)}
+                                    maxLength={2}
+                                    disabled={timeOff}
+                                />
+                            </div>
+                        </div>
+                        <div>
+                            <p className="text-center pb-2">الانصراف:</p>
+                            <div className="flex items-center gap-x-3">
+                                <input
+                                    placeholder="الساعة"
+                                    className="px-4 py-2 bg-comp rounded-xl w-20 disabled:bg-black/50" 
+                                    type="text"
+                                    name="leave_hour"
+                                    onKeyDown={e => handleNumInput(e, setLeaveHour)}
+                                    onChange={e => setLeaveHour(e.target.value)}
+                                    maxLength={2}
+                                    disabled={timeOff}
+                                    />
+                                <input
+                                    placeholder="الدقيقة"
+                                    className="px-4 py-2 bg-comp rounded-xl w-20 disabled:bg-black/50" 
+                                    type="text"
+                                    name="leave_minute"
+                                    onKeyDown={e => handleNumInput(e, setLeaveMin)}
+                                    onChange={e => setLeaveMin(e.target.value)}
+                                    maxLength={2}
+                                    disabled={timeOff}
+                                />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="pt-7">
+                    <p className="text-center pb-1 font-bold">وقت الخروج الضمني:</p>
+                    <div className="w-full flex items-center gap-x-10">
+                        <div>
+                            <p className="text-center pb-2">الخروج:</p>
+                            <div className="flex items-center gap-x-3">
+                                <input
+                                    placeholder="الساعة"
+                                    className="px-4 py-2 bg-comp rounded-xl w-20 disabled:bg-black/50" 
+                                    type="text"
+                                    name="exit_hour"
+                                    onKeyDown={e => handleNumInput(e, setExitHour)}
+                                    onChange={e => setExitHour(e.target.value)}
+                                    maxLength={2}
+                                    disabled={timeOff}
+                                    />
+                                <input
+                                    placeholder="الدقيقة"
+                                    className="px-4 py-2 bg-comp rounded-xl w-20 disabled:bg-black/50" 
+                                    type="text"
+                                    name="exit_minute"
+                                    onKeyDown={e => handleNumInput(e, setExitMin)}
+                                    onChange={e => setExitMin(e.target.value)}
+                                    maxLength={2}
+                                    disabled={timeOff}
+                                />
+                            </div>
+                        </div>
+                        <div>
+                            <p className="text-center pb-2">العودة:</p>
+                            <div className="flex items-center gap-x-3">
+                                <input
+                                    placeholder="الساعة"
+                                    className="px-4 py-2 bg-comp rounded-xl w-20 disabled:bg-black/50" 
+                                    type="text"
+                                    name="enter_hour"
+                                    onKeyDown={e => handleNumInput(e, setLeaveHour)}
+                                    onChange={e => setLeaveHour(e.target.value)}
+                                    maxLength={2}
+                                    disabled={timeOff}
+                                    />
+                                <input
+                                    placeholder="الدقيقة"
+                                    className="px-4 py-2 bg-comp rounded-xl w-20 disabled:bg-black/50" 
+                                    type="text"
+                                    name="enter_minute"
+                                    onKeyDown={e => handleNumInput(e, setLeaveMin)}
+                                    onChange={e => setLeaveMin(e.target.value)}
+                                    maxLength={2}
+                                    disabled={timeOff}
+                                />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="pt-7">
+                    <p className="text-center pb-2 font-bold">تاريخ اليوم:</p>
+                    <div className="flex items-center gap-x-8">
+                        <input
+                            placeholder="اليوم"
+                            className="px-4 py-2 bg-comp rounded-xl w-44" 
+                            type="text"
+                            name="date_day"
+                            onKeyDown={e => handleNumInput(e, setDateDay)}
+                            onChange={e => setDateDay(e.target.value)}
+                            maxLength={2}
+                            />
+                        <input
+                            placeholder="الشهر"
+                            className="px-4 py-2 bg-comp rounded-xl w-44" 
+                            type="text"
+                            name="date_month"
+                            onKeyDown={e => handleNumInput(e, setDateMonth)}
+                            onChange={e => setDateMonth(e.target.value)}
+                            maxLength={2}
+                        />
+                    </div>
+                    <textarea
+                        className="px-4 py-2 bg-comp rounded-xl resize-none w-full mt-5 h-20"
+                        placeholder="الملاحظات" 
+                        onChange={e => setNote(e.target.value)}
+                        name="note" 
+                    ></textarea>
+                </div>
                 <button type="submit" hidden />
             </form>
         </Modal>
