8a6005288b7b1833e2d9c8c91aa7a492cce4e346
realdaly <0you.hearty0@gmail.com>
2025-05-02 13:28:39 +0300

    add exit and enter during work time labels to DayRow component

--- components/profilepage/DayRow.jsx
+++ components/profilepage/DayRow.jsx
@@ -6,88 +6,110 @@ import ShowNoteBtn from "@/components/profilepage/ShowNoteBtn";
 
 export default function DayRow({data, year, month, getDays, groupInfo, timeOff}){
     return(
-        <div className="flex flex-col justify-center md:flex-row border-b border-r border-l last:rounded-b-xl bg-white odd:bg-domI relative">
-            <div className="p-3 my-auto text-center md:w-[14.28%] font-medium">
-                <div>{data.title}</div>
-                <div className="text-sm text-gray-500">
-                    {data.date_day}/{data.date_month}/{year.title}
+        <div className="border-b border-r border-l last:rounded-b-xl bg-white odd:bg-domI">
+            <div className="flex flex-col justify-center md:flex-row relative">
+                <div className="p-3 my-auto text-center md:w-[14.28%] font-medium">
+                    <div>{data.title}</div>
+                    <div className="text-sm text-gray-500">
+                        {data.date_day}/{data.date_month}/{year.title}
+                    </div>
                 </div>
+                <div className="p-3 my-auto text-center md:w-[14.28%]">
+                    {timeOff != 1 && <>
+                    الساعة 
+                    <span className="font-bold px-1">{data.attend_hour}</span>
+                    و
+                    <span className="font-bold px-1">{data.attend_minute}</span>
+                    دقيقة
+                    </>}
+                    {timeOff == 1 && <>
+                    <span className="font-semibold bg-orange-200 bg-opacity-50 rounded-full px-3">
+                        يوم إجازة
+                    </span>
+                    </>}
+                </div>
+                <div className="p-3 my-auto text-center md:w-[14.28%]">
+                    {timeOff != 1 && <>
+                    الساعة 
+                    <span className="font-bold px-1">{data.leave_hour}</span>
+                    و
+                    <span className="font-bold px-1">{data.leave_minute}</span>
+                    دقيقة
+                    </>}
+                </div>
+                <div className="p-3 my-auto text-center md:w-[14.28%]">
+                    {timeOff != 1 && <>
+                    <span className="font-bold px-1">{data.total_hours}</span>
+                    ساعة و
+                    <span className="font-bold px-1">{data.total_minutes}</span>
+                    دقيقة
+                    </>}
+                </div>
+                <div className="p-3 my-auto text-center md:w-[14.28%]">
+                    {timeOff != 1 && <>
+                    <span className="px-3 py-1 rounded-full bg-green-100">
+                        <span className="font-bold px-1">{data.more_hours}</span>
+                        ساعة و
+                        <span className="font-bold px-1">{data.more_minutes}</span>
+                        دقيقة
+                    </span>
+                    </>}
+                </div>
+                <div className="p-3 my-auto text-center md:w-[14.28%]">
+                    {timeOff != 1 && <>
+                    <span className="px-3 py-1 rounded-full bg-red-100">
+                        <span className="font-bold px-1">{data.less_hours}</span>
+                        ساعة و
+                        <span className="font-bold px-1">{data.less_minutes}</span>
+                        دقيقة
+                    </span>
+                    </>}
+                </div>
+                <div className="p-3 pr-9 my-auto text-center md:w-[14.28%]">
+                    {data.note ? 
+                        <ShowNoteBtn note={data.note} /> 
+                    : "......."}
+                </div>
+                <DropdownMenu
+                    button={<OptionsBtn />}
+                    menuStyle="absolute left-2 top-5"
+                    panelStyle="pl-5"
+                >
+                    <div className="bg-domI border rounded-lg overflow-hidden">
+                        <UpdateDayBtn 
+                            year={year}
+                            month={month}
+                            groupInfo={groupInfo}
+                            getDays={getDays}
+                            currentDay={data}
+                        />
+                        <DeleteDayBtn 
+                            currentDay={data}
+                            getDays={getDays}
+                        />
+                    </div>
+                </DropdownMenu>
             </div>
-            <div className="p-3 my-auto text-center md:w-[14.28%]">
-                {timeOff != 1 && <>
-                الساعة 
-                <span className="font-bold px-1">{data.attend_hour}</span>
-                و
-                <span className="font-bold px-1">{data.attend_minute}</span>
-                دقيقة
-                </>}
-                {timeOff == 1 && <>
+            {(data?.exit_hour && data?.exit_hour != "" && data?.exit_hour != 0) &&
+            <div className="pb-3 pr-5 flex items-center gap-x-3">
                 <span className="font-semibold bg-orange-200 bg-opacity-50 rounded-full px-3">
-                    يوم إجازة
+                    وقت الخروج الضمني:
                 </span>
-                </>}
-            </div>
-            <div className="p-3 my-auto text-center md:w-[14.28%]">
-                {timeOff != 1 && <>
-                الساعة 
-                <span className="font-bold px-1">{data.leave_hour}</span>
-                و
-                <span className="font-bold px-1">{data.leave_minute}</span>
-                دقيقة
-                </>}
-            </div>
-            <div className="p-3 my-auto text-center md:w-[14.28%]">
-                {timeOff != 1 && <>
-                <span className="font-bold px-1">{data.total_hours}</span>
-                ساعة و
-                <span className="font-bold px-1">{data.total_minutes}</span>
-                دقيقة
-                </>}
-            </div>
-            <div className="p-3 my-auto text-center md:w-[14.28%]">
-                {timeOff != 1 && <>
-                <span className="px-3 py-1 rounded-full bg-green-100">
-                    <span className="font-bold px-1">{data.more_hours}</span>
-                    ساعة و
-                    <span className="font-bold px-1">{data.more_minutes}</span>
+                <span className="px-3 rounded-full bg-red-100">
+                    خروج عند الساعة
+                    <span className="font-bold px-1">{data.exit_hour}</span>
+                    و
+                    <span className="font-bold px-1">{data.exit_minutes}</span>
                     دقيقة
                 </span>
-                </>}
-            </div>
-            <div className="p-3 my-auto text-center md:w-[14.28%]">
-                {timeOff != 1 && <>
-                <span className="px-3 py-1 rounded-full bg-red-100">
-                    <span className="font-bold px-1">{data.less_hours}</span>
-                    ساعة و
-                    <span className="font-bold px-1">{data.less_minutes}</span>
+                <span className="px-3 rounded-full bg-green-100">
+                    عودة عند الساعة
+                    <span className="font-bold px-1">{data.enter_hour}</span>
+                    و
+                    <span className="font-bold px-1">{data.enter_minutes}</span>
                     دقيقة
                 </span>
-                </>}
-            </div>
-            <div className="p-3 pr-9 my-auto text-center md:w-[14.28%]">
-                {data.note ? 
-                    <ShowNoteBtn note={data.note} /> 
-                : "......."}
-            </div>
-            <DropdownMenu
-                button={<OptionsBtn />}
-                menuStyle="absolute left-2 top-5"
-                panelStyle="pl-5"
-            >
-                <div className="bg-domI border rounded-lg overflow-hidden">
-                    <UpdateDayBtn 
-                        year={year}
-                        month={month}
-                        groupInfo={groupInfo}
-                        getDays={getDays}
-                        currentDay={data}
-                    />
-                    <DeleteDayBtn 
-                        currentDay={data}
-                        getDays={getDays}
-                    />
-                </div>
-            </DropdownMenu>
+            </div>}
         </div>
     )
 }
--- components/profilepage/TableHeader.jsx
+++ components/profilepage/TableHeader.jsx
@@ -1,6 +1,6 @@
 export default function TableHeader(){
     return(
-        <div className="flex flex-col md:flex-row bg-gray-200 border-b font-bold rounded-t-xl">
+        <div className="sticky flex flex-col md:flex-row bg-gray-200 border-b font-bold rounded-t-xl">
             <div className="p-3 text-center md:w-[14.28%]">اليوم</div>
             <div className="p-3 text-center md:w-[14.28%]">وقت الحضور</div>
             <div className="p-3 text-center md:w-[14.28%]">وقت الانصراف</div>
